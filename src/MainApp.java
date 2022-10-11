8a0172022380db6887585a254765d72c226bff01
isakrc52 <114700918+isakrc52@users.noreply.github.com>
2022-10-11 20:27:28 +0300

    Dünya değişti

---
+++ src/MainApp.java
@@ -0,0 +1,16 @@
+/**
+ * başlangıç sınıfıdır
+ * @link
+ * @see
+ */
+public class MainApp {
+    //Bu metot programı başlatır
+
+
+    public static void main(String[] args) {
+          System.out.println("Hiii");
+        System.out.println();
+        //TODO: BU KISIMDA TOPLAMA İŞLEMİ YAPILACAK
+        System.out.println("merhaba dünya");
+    }
+}
